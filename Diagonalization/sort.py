037cfe0914e317b5b987e544f7f857fb085df7f4
BuildTools <unconfigured@null.spigotmc.org>
2024-12-06 17:54:14 +0100

    Update First Commit

--- Diagonalization/sort.py
+++ Diagonalization/sort.py
@@ -2,14 +2,14 @@ def triFusion(L):
     if len(L) == 1:
         return L
     else:
-        return fusion( triFusion(L[:len(L)//2]) , triFusion(L[len(L)//2:]) )
+        return fusion(triFusion(L[:len(L)//2]) , triFusion(L[len(L)//2:]))
     
-def fusion(A,B):
+def fusion(A, B):
     if len(A) == 0:
         return B
     elif len(B) == 0:
         return A
     elif A[0] <= B[0]:
-        return [A[0]] + fusion( A[1:] , B )
+        return [A[0]] + fusion(A[1:], B)
     else:
-        return [B[0]] + fusion( A , B[1:] )
+        return [B[0]] + fusion(A, B[1:])
